0a48ef9127e2c82cf818771f69e852960ab66c2d
niddle <haus2013@yandex.ru>
2021-03-30 18:54:02 +0500

    Now it works.

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
+#include <config.h>
 
 int main()
 {
 	std::cout << "Who are you ? ";
 	std::string name;
 	std::getline(std::cin, name);
-	std::cout << "Hello, " << name << ", from greeter v1.0" << std::endl;
+	std::cout << "Hello, " << name << ", from "<<
+	PACKAGE_STRING << " !" << std::endl;
 
 	return 0;
 }
